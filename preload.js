9697845fb119310dc93e3cf4eedb0203f056cc68
Aleksandr <keliberda99@yandex.ru>
2025-03-22 15:30:01 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-console.log('Loading preload.js...');
-
 contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.invoke('minimize-window'),
   maximizeWindow: () => ipcRenderer.invoke('maximize-window'),
   closeWindow: () => ipcRenderer.invoke('close-window'),
   onWindowRestored: (callback) => ipcRenderer.on('window-restored', callback),
+  onWindowMaximized: (callback) => ipcRenderer.on('window-maximized', callback),
+  onWindowUnmaximized: (callback) => ipcRenderer.on('window-unmaximized', callback),
 });
