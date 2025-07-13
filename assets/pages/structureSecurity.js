a97f46257bb2b266800865cee0c4e27f8d4b11d1
Aleksandr <keliberda99@yandex.ru>
2025-07-13 14:48:22 +0800

    Update structureSecurity.js

--- assets/pages/structureSecurity.js
+++ assets/pages/structureSecurity.js
@@ -1,5 +1,8 @@
-  function loadStructureSecurityContent(container) {
-    container.innerHTML = `
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0.
+
+function loadStructureSecurityContent(container) {
+container.innerHTML = `
 <div class="structure-security-container">
       <h1>Защита структур</h1>
 
