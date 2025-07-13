9281482fae11b46baf97b6d39d7f46be4f13e461
Aleksandr <keliberda99@yandex.ru>
2025-07-13 14:45:04 +0800

    Update search.js

--- assets/search.js
+++ assets/search.js
@@ -1,3 +1,6 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0.
+
 function extractTextFromElement(element, maxLength = 10000) {
   let text = '';
   if (element.nodeType === Node.TEXT_NODE) {
