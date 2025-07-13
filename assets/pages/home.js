cd5fcc4ae594d7be8063351bc39913ea995e53cb
Aleksandr <keliberda99@yandex.ru>
2025-07-13 14:46:19 +0800

    Update home.js

--- assets/pages/home.js
+++ assets/pages/home.js
@@ -1,3 +1,6 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0.
+
 function loadHomeContent(targetArea) {
   targetArea.innerHTML = `
     <h1>Главная</h1>
