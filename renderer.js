7ccd34b83fd7c3f923ca36f9cbc7620081eb7bca
Aleksandr <keliberda99@yandex.ru>
2025-07-13 14:43:57 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0.
+
 document.addEventListener('DOMContentLoaded', () => {
   const windowContainer = document.getElementById('window-container');
   const contentArea = document.getElementById('content-area');
